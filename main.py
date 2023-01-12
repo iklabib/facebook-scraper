731ebd4fba617662bb39fc84601767431aa1eadb
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2023-01-12 17:55:51 +0700

    update author info and refactor scraping

--- main.py
+++ main.py
@@ -6,24 +6,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-def smooth_scroll(current_scroll_position = 0):
+def smooth_scroll(current_scroll_position = 0, iteration_multiplyer = 1):
     driver.execute_script("document.documentElement.style.setProperty('scroll-behavior', 'smooth');")    
     scroll_iteration = 60 #default 60
-    for _ in range(scroll_iteration):
+    for _ in range(int(scroll_iteration*iteration_multiplyer)):
         current_scroll_position += 100
         driver.execute_script(f"window.scrollTo(0, {current_scroll_position});")
         time.sleep(0.1)
         new_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
     return current_scroll_position
 
+def export(data):
+    df = pd.DataFrame(data)
+    df.to_csv("output/scraping2.csv", index=False, header=True)
 
-#Initiate chrome driver
+def display(data):
+    for item in data:
+        print('========================')
+        print(item[0])
+        print(item[1])
+        print()
+
 options = Options()
 options.add_experimental_option("detach", False)
 
 driver = webdriver.Chrome(options=options)
-
-# Go to website
 driver.get("https://web.facebook.com/groups/306516256927109")
 
 
@@ -36,44 +43,42 @@ except NoSuchElementException:
 try:
     element = driver.find_element(By.XPATH, "//*[text()='Public group']")
     class_name = element.get_attribute("class")
-    # print(class_name)
 except NoSuchElementException:
     print("Class not found")
 
 arr = [
-    'x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r x126k92a'
+    'x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r x126k92a',
     'x11i5rnm xat24cr x1mh8g0r x1vvkbs xtlvy1s x126k92a',
-    'x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r x126k92a'
 ]
+
 try:
-    current_scroll_position = 0
     data = []
-    for _ in range(5):
+    list_author = []
+    print('loading...')
+    time.sleep(2)
+    current_scroll_position = 0
+    feed = driver.find_elements(By.CSS_SELECTOR, "div[role='feed']")
+    feed = feed[0]
+    for _ in range(3):
         print('starting a new batch...')
         current_scroll_position = smooth_scroll(current_scroll_position)
-        elements = driver.find_elements(By.CLASS_NAME, "x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.x126k92a")
-        index = 1
-        string = ''
-        for element in elements:
-            class_name = element.get_attribute("class")
-            if 'xdj266r' in class_name:
-                if string not in data and string != '' and string != '\n':
-                    data.append(string)
-                    string = ''
-                string = element.text
-                index+=1
-            else:
-                string += f'\n{element.text}'
-            
-    for index, text in enumerate(data): 
-        print(f'{index}. {text}\n')
 
-    df = pd.DataFrame({'data': data})
-    df.to_csv("output/scraping.csv", index=False, header=True)
+        cards = feed.find_elements(By.CLASS_NAME, "x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z")
+        for card in cards:
+            author = card.find_elements(By.CLASS_NAME, "x1heor9g.x1qlqyl8.x1pd3egz.x1a2a7pz.x1gslohp.x1yc453h")
+            author = author[0].text
+            post = card.find_elements(By.CLASS_NAME, "x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.x126k92a")
+            post = [p.text for p in post]
+            post = '\n'.join(post)
+            if not any(post in row for row in data) and author != '' and author != '\n':
+                data.append([author,post])
+
+    display(data)
+
+    export(data)
 
     
 except NoSuchElementException:
     print("Content not found")
 
-# Close the browser
 driver.quit()
